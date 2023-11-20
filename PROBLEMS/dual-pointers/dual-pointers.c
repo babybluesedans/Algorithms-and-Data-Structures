fc20bed04601ce6e780b869004b95763b7976fa7
Josh Taylor <140925216+babybluesedans@users.noreply.github.com>
2023-11-20 05:26:00 +0000

    dual pointers

--- PROBLEMS/dual-pointers/dual-pointers.c
+++ PROBLEMS/dual-pointers/dual-pointers.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 1000000
-unsigned int longestPrefix(char s1[], char s2[]);
-unsigned int longestSuffix(char s1[], char s2[], unsigned int len);
+#define MAX 1000002
+int longestPrefix(char s1[], char s2[], int len);
+int longestSuffix(char s1[], char s2[], int len);
 
 int main(void)
 {
@@ -12,41 +12,54 @@ int main(void)
     fgets(&s1[0], MAX, stdin);
     fgets(&s2[0], MAX, stdin);
 
-    stringLen = strlen(s1)
+    int s1Len = strlen(s1);
+    int s2Len = strlen(s2);
 
-    unsigned int longestPref = longestPrefix(s1, s2); //below this bound + 1
-    unsigned int longestSuff = longestSuffix(s1, s2, stringLen); //above this bound
+    int longestPref = longestPrefix(s1, s2, s2Len); //below this bound + 1
+    int longestSuff = longestSuffix(s1, s2, s1Len); //above this bound
 
-    for (int i = longestSuff + 1; i < longestPref + 2; i++);
+    int total = (longestPref + 1) - (longestSuff );
+    if (total <= 0)
     {
-        
-        int j;
-        int k;
-        for (j = 0; j < i; j++)//create new prefix
-        {
+        total = 0;
+    }
+    if (s1Len > MAX || s2Len > MAX)
+    {
+        total = 0;
+    }
 
+    
+    
+    printf("%u\n", total);
+
+    if (total != 0)
+    {
+        for (int i = 0; i < total; i++)
+        {
+            printf("%u ", longestSuff + 1 + i);
         }
+        printf("\n");
     }
 
 
 }
 
-unsigned int longestPrefix(char s1[], char s2[])
+int longestPrefix(char s1[], char s2[], int len)
 {
-    unsigned int i = 1;
-    while (s1[i] == s2[i] && i < MAX + 2)
+    int i = 0;
+    while (s1[i] == s2[i] && i < MAX  && i < len - 1)
     {
         i++;
     }
+
     return i;
 }
 
-unsigned int longestSuffix(char s1[], char s2[], unsigned int len)
+int longestSuffix(char s1[], char s2[], int len)
 {
-    unsigned int i = len;
-    printf("%u\n", len);
+    int i = len - 2;
 
-    while(s1[i] == s2[i - 1] && i > 2)
+    while(i > 0 && s1[i] == s2[i - 1] && i < MAX)
     {
         i--;
     }
